3f62ef3e4d8cdc83b6fc0e9ea536ff12bc2cad09
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2023-06-26 16:36:42 -0700

    Update lune.rb

--- Formula/lune.rb
+++ Formula/lune.rb
@@ -5,10 +5,6 @@ class Lune < Formula
   sha256 "dd8fac5f2dad70b4da50df61abf97b38604f93985e52e863dfddbfc26c1b5e83"
   license "MPL-2.0"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c8a406a7c7e70252f987e7104c608fffdf9cc7d7df15485b8d4fbe366ed62b8f"
-  end
-
   depends_on "rust" => :build
 
   def install
