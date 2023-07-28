1893d72bb213103a7c83755be44b60088294ef28
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2023-07-28 12:19:53 -0700

    Updated to new docs

--- Formula/lune.rb
+++ Formula/lune.rb
@@ -1,6 +1,6 @@
 class Lune < Formula
   desc "Standalone Luau script runtime 🌙"
-  homepage "https://lune.gitbook.io/"
+  homepage "https://lune-org.github.io/docs"
   url "https://github.com/filiptibell/lune/archive/refs/tags/v0.7.5.tar.gz"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "MPL-2.0"
