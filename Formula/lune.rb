d7d7d75ce8d00d94aa7173d7ec4831eeb2bd5997
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2023-07-28 12:20:43 -0700

    Fixed sha256 mismatch

--- Formula/lune.rb
+++ Formula/lune.rb
@@ -2,7 +2,7 @@ class Lune < Formula
   desc "Standalone Luau script runtime 🌙"
   homepage "https://lune-org.github.io/docs"
   url "https://github.com/filiptibell/lune/archive/refs/tags/v0.7.5.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "e8191df5d6844026772cc7afab1083235a265c506474c4c4dee0a7724b04f775"
   license "MPL-2.0"
 
   depends_on "rust" => :build
