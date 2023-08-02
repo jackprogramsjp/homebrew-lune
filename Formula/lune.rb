4b9e6c0869e8cf70c7866a3d2b341186d08d6a3f
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2023-08-01 22:38:53 -0700

    Removed unicode emojis

--- Formula/lune.rb
+++ Formula/lune.rb
@@ -1,5 +1,5 @@
 class Lune < Formula
-  desc "Standalone Luau script runtime 🌙"
+  desc "Standalone Luau script runtime"
   homepage "https://lune-org.github.io/docs"
   url "https://github.com/filiptibell/lune/archive/refs/tags/v0.7.5.tar.gz"
   sha256 "e8191df5d6844026772cc7afab1083235a265c506474c4c4dee0a7724b04f775"
